a6c8a3a222dbe42b5c869b20c2481f14e6c83ee5
Jack Cook <hello@jackcook.com>
2026-01-22 17:28:05 -0500

    Re-implement MXFP4 quantization

--- src/fouroversix/quantize/triton_kernel.py
+++ src/fouroversix/quantize/triton_kernel.py
@@ -70,7 +70,131 @@ def rht_kernel(
 
 
 @triton.jit
-def fp32_to_scaled_fp4_kernel_fouroversix(  # noqa: C901, PLR0912
+def block_scaled_fp4_quantization_kernel(
+    x_block,
+    x_amax_ptr,
+    BLOCK_SIZE_M: tl.constexpr,
+    BLOCK_SIZE_N: tl.constexpr,
+    FP4_FORMAT: tl.constexpr,
+    ROUND_STYLE: tl.constexpr,
+    BLOCK_SCALE_2D: tl.constexpr,
+    SCALE_RULE: tl.constexpr,
+) -> None:
+    E2M1_MAX_ALLOWED_VALUE: tl.constexpr = (
+        E2M1_MAX_VALUE if SCALE_RULE == SCALE_RULE_ALWAYS_6 else E2M1_MAX_FOUR
+    )
+
+    if FP4_FORMAT == FP4_FORMAT_MXFP4:
+        x_scale_blocks = x_block.reshape(128, 4, 32)
+        x_scales_hp = tl.max(x_scale_blocks.abs(), axis=-1) / E2M1_MAX_ALLOWED_VALUE
+        x_scales_e8m0_u32 = x_scales_hp.cast(tl.uint32, bitcast=True)
+
+        # Use the 8-bit exponent as the scale factor
+        x_scales_e8m0 = ((x_scales_e8m0_u32 >> 23) & 0xFF).to(tl.uint8)
+
+        # Add one in order to round up
+        x_scales = tl.where(
+            (x_scales_e8m0_u32 & 0x7FFFFF) == 0,
+            x_scales_e8m0,
+            x_scales_e8m0 + 1,
+        )
+
+        # Convert the rounded-up scale factor back to a 32-bit float
+        x_scales_hp = (x_scales.cast(tl.uint32) << 23).cast(x_block.dtype, bitcast=True)
+
+        if BLOCK_SCALE_2D:
+            x_scales_hp = (
+                tl.max(
+                    x_scales_hp.reshape(4, 32, 4).permute(0, 2, 1),
+                    axis=-1,
+                )
+                .expand_dims(0)
+                .broadcast_to(4, 32, 4)
+                .permute(1, 0, 2)
+                .reshape(128, 4)
+            )
+
+        (x_block_scaled_b1, x_block_scaled_b2) = (
+            (x_scale_blocks / x_scales_hp.expand_dims(2))
+            .reshape(BLOCK_SIZE_M, BLOCK_SIZE_N // 2, 2)
+            .split()
+        )
+    elif FP4_FORMAT == FP4_FORMAT_NVFP4:
+        x_amax = tl.load(x_amax_ptr)
+        x_scale_blocks = x_block.reshape(128, 4, 16)
+        x_scales_hp = tl.max(x_scale_blocks.abs(), axis=-1) * E4M3_MAX_VALUE / x_amax
+
+        if BLOCK_SCALE_2D:
+            x_scales_hp = (
+                tl.max(
+                    x_scales_hp.reshape(8, 16, 4).permute(0, 2, 1),
+                    axis=-1,
+                )
+                .expand_dims(0)
+                .broadcast_to(16, 8, 4)
+                .permute(1, 0, 2)
+                .reshape(128, 4)
+            )
+
+        x_scales = x_scales_hp.to(tl.float8e4nv)
+
+        (x_block_scaled_b1, x_block_scaled_b2) = (
+            tl.where(
+                x_scales.expand_dims(2).to(x_amax.dtype) != 0,
+                (x_scale_blocks * E2M1_MAX_ALLOWED_VALUE * E4M3_MAX_VALUE)
+                / (x_amax * x_scales.to(x_amax.dtype).expand_dims(2)),
+                0,
+            )
+            .reshape(BLOCK_SIZE_M, BLOCK_SIZE_N // 2, 2)
+            .split()
+        )
+
+    if ROUND_STYLE == ROUND_STYLE_NEAREST:
+        x_e2m1 = tl.inline_asm_elementwise(
+            asm="""
+                {
+                .reg .b8 byte0, byte1, byte2, byte3;
+                cvt.rn.satfinite.e2m1x2.f32 byte0, $5, $1;
+                cvt.rn.satfinite.e2m1x2.f32 byte1, $6, $2;
+                cvt.rn.satfinite.e2m1x2.f32 byte2, $7, $3;
+                cvt.rn.satfinite.e2m1x2.f32 byte3, $8, $4;
+                mov.b32 $0, {byte0, byte1, byte2, byte3};
+                }
+                """,
+            constraints="=r,r,r,r,r,r,r,r,r",
+            args=[x_block_scaled_b1, x_block_scaled_b2],
+            dtype=tl.uint8,
+            is_pure=True,
+            pack=4,
+        )
+    elif ROUND_STYLE == ROUND_STYLE_STOCHASTIC:
+        rbits = tl.rand(
+            0,
+            tl.arange(0, BLOCK_SIZE_M)[:, None] * BLOCK_SIZE_N // 2
+            + tl.arange(0, BLOCK_SIZE_N // 2)[None, :],
+        ).cast(tl.uint32, bitcast=True)
+
+        x_e2m1 = tl.inline_asm_elementwise(
+            asm="""
+            {
+            .reg .b16 tmp0, tmp1;
+            cvt.rs.satfinite.e2m1x4.f32 tmp0, {$6, $2, $5, $1}, $9;
+            cvt.rs.satfinite.e2m1x4.f32 tmp1, {$8, $4, $7, $3}, $10;
+            mov.b32 $0, {tmp0, tmp1};
+            }
+            """,
+            constraints="=r,r,r,r,r,r,r,r,r,r,r,r,r",
+            args=[x_block_scaled_b1, x_block_scaled_b2, rbits],
+            dtype=tl.uint8,
+            is_pure=True,
+            pack=4,
+        )
+
+    return x_e2m1, x_scales.reshape(4, 32, 4).permute(1, 0, 2).ravel()
+
+
+@triton.jit
+def nvfp4_fouroversix_quantization_kernel(
     x_block,
     x_amax_ptr,
     BLOCK_SIZE_M: tl.constexpr,
@@ -81,16 +205,7 @@ def fp32_to_scaled_fp4_kernel_fouroversix(  # noqa: C901, PLR0912
 ) -> None:
     x_amax = tl.load(x_amax_ptr)
     x_scale_blocks = x_block.reshape(128, 4, 16)
-
-    # Calculate six blocks
-    if (
-        SCALE_RULE == SCALE_RULE_ALWAYS_6 or SCALE_RULE == SCALE_RULE_ALWAYS_4
-    ):  # noqa: SIM109, PLR1714
-        x_scales_hp = tl.max(x_scale_blocks.abs(), axis=-1) * E4M3_MAX_VALUE / x_amax
-    else:
-        x_scales_hp = (
-            tl.max(x_scale_blocks.abs(), axis=-1) * E4M3_MAX_FOUROVERSIX / x_amax
-        )
+    x_scales_hp = tl.max(x_scale_blocks.abs(), axis=-1) * E4M3_MAX_FOUROVERSIX / x_amax
 
     if BLOCK_SCALE_2D:
         x_scales_hp = (
@@ -107,56 +222,27 @@ def fp32_to_scaled_fp4_kernel_fouroversix(  # noqa: C901, PLR0912
     x_scales_6 = x_scales_hp.to(tl.float8e4nv)
     x_scales_4 = (x_scales_hp * (6 / 4)).to(tl.float8e4nv)
 
-    if SCALE_RULE == SCALE_RULE_ALWAYS_4:
-        x_scales_6 = (x_scales_hp * (4 / 6)).to(tl.float8e4nv)
-        x_scales_4 = x_scales_hp.to(tl.float8e4nv)
+    (x_block_scaled_6_b1, x_block_scaled_6_b2) = (
+        tl.where(
+            x_scales_6.expand_dims(2).to(x_amax.dtype) != 0,
+            (x_scale_blocks * E2M1_MAX_VALUE * E4M3_MAX_FOUROVERSIX)
+            / (x_amax * x_scales_6.to(x_amax.dtype).expand_dims(2)),
+            0,
+        )
+        .reshape(BLOCK_SIZE_M, BLOCK_SIZE_N // 2, 2)
+        .split()
+    )
 
-    if (
-        SCALE_RULE == SCALE_RULE_ALWAYS_6 or SCALE_RULE == SCALE_RULE_ALWAYS_4
-    ):  # noqa: SIM109, PLR1714
-        (x_block_scaled_6_b1, x_block_scaled_6_b2) = (
-            tl.where(
-                x_scales_6.expand_dims(2).to(x_amax.dtype) != 0,
-                (x_scale_blocks * E2M1_MAX_VALUE * E4M3_MAX_VALUE)
-                / (x_amax * x_scales_6.to(x_amax.dtype).expand_dims(2)),
-                0,
-            )
-            .reshape(BLOCK_SIZE_M, BLOCK_SIZE_N // 2, 2)
-            .split()
+    (x_block_scaled_4_b1, x_block_scaled_4_b2) = (
+        tl.where(
+            x_scales_4.expand_dims(2).to(x_amax.dtype) != 0,
+            (x_scale_blocks * E2M1_MAX_VALUE * E4M3_MAX_FOUROVERSIX)
+            / (x_amax * x_scales_4.to(x_amax.dtype).expand_dims(2)),
+            0,
         )
-
-        (x_block_scaled_4_b1, x_block_scaled_4_b2) = (
-            tl.where(
-                x_scales_4.expand_dims(2).to(x_amax.dtype) != 0,
-                (x_scale_blocks * E2M1_MAX_FOUR * E4M3_MAX_VALUE)
-                / (x_amax * x_scales_4.to(x_amax.dtype).expand_dims(2)),
-                0,
-            )
-            .reshape(BLOCK_SIZE_M, BLOCK_SIZE_N // 2, 2)
-            .split()
-        )
-    else:
-        (x_block_scaled_6_b1, x_block_scaled_6_b2) = (
-            tl.where(
-                x_scales_6.expand_dims(2).to(x_amax.dtype) != 0,
-                (x_scale_blocks * E2M1_MAX_VALUE * E4M3_MAX_FOUROVERSIX)
-                / (x_amax * x_scales_6.to(x_amax.dtype).expand_dims(2)),
-                0,
-            )
-            .reshape(BLOCK_SIZE_M, BLOCK_SIZE_N // 2, 2)
-            .split()
-        )
-
-        (x_block_scaled_4_b1, x_block_scaled_4_b2) = (
-            tl.where(
-                x_scales_4.expand_dims(2).to(x_amax.dtype) != 0,
-                (x_scale_blocks * E2M1_MAX_VALUE * E4M3_MAX_FOUROVERSIX)
-                / (x_amax * x_scales_4.to(x_amax.dtype).expand_dims(2)),
-                0,
-            )
-            .reshape(BLOCK_SIZE_M, BLOCK_SIZE_N // 2, 2)
-            .split()
-        )
+        .reshape(BLOCK_SIZE_M, BLOCK_SIZE_N // 2, 2)
+        .split()
+    )
 
     if ROUND_STYLE == ROUND_STYLE_NEAREST:
         (x_e2m1_6, x_e2m1_4, x_fp16x2_6, x_fp16x2_4) = tl.inline_asm_elementwise(
@@ -411,15 +497,29 @@ def fp4_quantization_kernel(
     else:
         x_block = x_desc.load([n_block_offset, m_block_offset]).T
 
-    x_e2m1, x_scales = fp32_to_scaled_fp4_kernel_fouroversix(
-        x_block.to(tl.float32),
-        x_amax_ptr,
-        BLOCK_SIZE_M,
-        BLOCK_SIZE_N,
-        ROUND_STYLE,
-        BLOCK_SCALE_2D,
-        SCALE_RULE,
-    )
+    x_block = x_block.to(tl.float32)
+
+    if SCALE_RULE == SCALE_RULE_ALWAYS_6 or SCALE_RULE == SCALE_RULE_ALWAYS_4:  # noqa: PLR1714, SIM109
+        x_e2m1, x_scales = block_scaled_fp4_quantization_kernel(
+            x_block,
+            x_amax_ptr,
+            BLOCK_SIZE_M,
+            BLOCK_SIZE_N,
+            FP4_FORMAT,
+            ROUND_STYLE,
+            BLOCK_SCALE_2D,
+            SCALE_RULE,
+        )
+    else:
+        x_e2m1, x_scales = nvfp4_fouroversix_quantization_kernel(
+            x_block,
+            x_amax_ptr,
+            BLOCK_SIZE_M,
+            BLOCK_SIZE_N,
+            ROUND_STYLE,
+            BLOCK_SCALE_2D,
+            SCALE_RULE,
+        )
 
     e2m1_n_block_offset = pid_n * BLOCK_SIZE_N // 2
     x_e2m1_desc.store([m_block_offset, e2m1_n_block_offset], x_e2m1)
@@ -460,6 +560,9 @@ def quantize_to_fp4(  # noqa: C901
 
         if x_amax is None:
             x_amax = x.abs().max().float()
+
+    # print(scale_dtype)
+    # print("===")
 
     padded_m = M + (block_size_m - M % block_size_m) % block_size_m
     padded_n = N + (block_size_n - N % block_size_n) % block_size_n
