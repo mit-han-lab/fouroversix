2ce76a4754038980a0f6fee9526e255bf78c76f1
Jack Cook <hello@jackcook.com>
2026-02-12 20:23:30 -0500

    Fix linting issues

--- scripts/hadamard_code_gen.py
+++ scripts/hadamard_code_gen.py
@@ -70,7 +70,7 @@ __device__ __forceinline__ void hadamard_mult_thread_{N}(float x[{N}]) {
 """
 
 
-def string_to_array(string):
+def string_to_array(string: str) -> np.ndarray:
     # Convert strings of + and - to bool arrays
     string = string.strip().replace("+", "1").replace("-", "-1").split()
     return np.stack(
@@ -81,19 +81,21 @@ def string_to_array(string):
     )
 
 
-def array_code_gen(arr):
-    N = arr.shape[0]
-    assert arr.shape[0] == arr.shape[1]
+def array_code_gen(arr: np.ndarray) -> str:
+    n = arr.shape[0]
+    if arr.shape[0] != arr.shape[1]:
+        msg = f"Hadamard matrix is not square: {arr.shape}"
+        raise ValueError(msg)
     out = [
         f"out[{i}] = "
-        + " ".join([f"{'+' if arr[i, j] == 1 else '-'} x[{j}]" for j in range(N)])
+        + " ".join([f"{'+' if arr[i, j] == 1 else '-'} x[{j}]" for j in range(n)])
         + ";"
-        for i in range(N)
+        for i in range(n)
     ]
-    return template.replace("{N}", str(N)).replace("{code}", "\n    ".join(out))
+    return template.replace("{N}", str(n)).replace("{code}", "\n    ".join(out))
 
 
-def main():
+def main() -> None:
     output_dir = (
         Path(__file__).parent.parent
         / "src"
